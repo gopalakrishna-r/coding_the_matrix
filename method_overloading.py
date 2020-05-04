61a9848da781b1ec20d0c9f054f062dcee349ba0
* <gopalakrishnar91@gmail.com>
2020-05-04 10:20:05 +0530

    initial commit

---
+++ method_overloading.py
@@ -0,0 +1,4 @@
+def solve(a,b,c): 
+	return (c-b)/a
+	
+print("solution for (10+5l)x + 5 = 20 is",solve(10+5j, 5, 20))	
---
+++ real_numbers.py
@@ -0,0 +1,3 @@
+x = 1+3j
+print("solution for x (x-1)**2 is",(x-1)**2)
+print( "imaginary part of x is %s and real part of x is %s" %(x.real, x.imag))
